5de3fcfb0e87a2878fcb972ca6c9b702bb297caf
Rassul <kenshihzhz@gmail.com>
2022-06-09 00:04:30 +0600

    Added Swagger annotations

--- src/main/java/kz/ks/storefront/addressbook/controller/AddressController.java
+++ src/main/java/kz/ks/storefront/addressbook/controller/AddressController.java
@@ -1,5 +1,14 @@
 package kz.ks.storefront.addressbook.controller;
 
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kz.ks.storefront.addressbook.controller.dto.AddressDTO;
 import kz.ks.storefront.addressbook.controller.dto.PersistentAddressDTO;
 import kz.ks.storefront.addressbook.converter.AddressModelPersistentConverter;
@@ -18,6 +27,8 @@ import java.util.Objects;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Addresses",
+        description = "CRUD operations on addresses")
 public class AddressController {
     private final AddressRepository addressRepository;
     private final CustomerRepository customerRepository;
@@ -25,8 +36,28 @@ public class AddressController {
     private final AddressModelPersistentConverter addressModelPersistentConverter;
 
     @GetMapping("/address/find")
-    public List<PersistentAddressDTO> getListByIdAndCityId(@RequestParam("cid") String customerId,
-                                                           @RequestParam("cityId") String cityId) {
+    @Operation(summary = "Get addresses by customer. ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success",
+                    content = {@Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = PersistentAddressDTO.class)))}),
+            @ApiResponse(responseCode = "404", description = "No such customer",
+                    content = @Content)})
+    public List<PersistentAddressDTO> getListByIdAndCityId(
+            @RequestParam("cid")
+            @Parameter(
+                    description = "Customer ID",
+                    example = "103395280",
+                    required = true
+            )
+            String customerId,
+            @RequestParam("cityID")
+            @Parameter(
+                    description = "City ID",
+                    example = "23",
+                    required = true
+            )
+            String cityId) {
         var existingCustomer = customerRepository.findByGci(customerId).orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
         );
@@ -36,7 +67,21 @@ public class AddressController {
     }
 
     @GetMapping("/address/{addressId}")
-    public PersistentAddressDTO findById(@PathVariable("addressId") long addressId) {
+    @Operation(summary = "Get address information by address-ID. ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PersistentAddressDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "No such address",
+                    content = @Content)})
+    public PersistentAddressDTO findById(
+            @PathVariable("addressID")
+            @Parameter(
+                    description = "address ID",
+                    example = "103",
+                    required = true
+            )
+            long addressId) {
         var existingAddress = addressRepository.findById(addressId);
 
         return existingAddress.map(
@@ -47,7 +92,20 @@ public class AddressController {
     }
 
     @PostMapping("/address")
-    public PersistentAddressDTO create(@RequestBody AddressDTO addressDTO) {
+    @Operation(summary = "Creating address by Address Data Transfer Object. ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PersistentAddressDTO.class))}),
+            @ApiResponse(responseCode = "400", description = "No such customer for owner field",
+                    content = @Content)})
+    public PersistentAddressDTO create(
+            @RequestBody
+            @Parameter(
+                    description = "Address Data Transfer Object",
+                    required = true
+            )
+            AddressDTO addressDTO) {
         var existingCustomer =
                 customerRepository.findByGci(addressDTO.getCustomerGci())
                         .orElseThrow(
@@ -64,7 +122,21 @@ public class AddressController {
     }
 
     @DeleteMapping("/address/{addressId}")
-    public void delete(@PathVariable("addressId") long addressId) {
+    @Operation(summary = "Deleting address by Address ID. ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = void.class))}),
+            @ApiResponse(responseCode = "404", description = "No such address",
+                    content = @Content)})
+    public void delete(
+            @PathVariable("addressId")
+            @Parameter(
+                    description = "Address ID",
+                    example = "678",
+                    required = true
+            )
+            long addressId) {
         addressRepository.findById(addressId)
                 .ifPresentOrElse(
                         addressRepository::delete,
@@ -74,8 +146,29 @@ public class AddressController {
     }
 
     @PostMapping("/address/{addressId}")
-    public void update(@PathVariable("addressId") long addressId,
-                       @RequestBody AddressDTO addressDTO) {
+    @Operation(summary = "Updating address by Address-ID and Address Data Transfer Object. ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = void.class))}),
+            @ApiResponse(responseCode = "400", description = "No such customer for owner field",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "No such address",
+                    content = @Content)})
+    public void update(
+            @PathVariable("addressId")
+            @Parameter(
+                    description = "Address ID",
+                    example = "9001",
+                    required = true
+            )
+            long addressId,
+            @RequestBody
+            @Parameter(
+                    description = "Address Data Transfer Object",
+                    required = true
+            )
+            AddressDTO addressDTO) {
         var existingCustomer =
                 customerRepository.findByGci(addressDTO.getCustomerGci())
                         .orElseThrow(
--- src/main/java/kz/ks/storefront/addressbook/controller/dto/PersistentCustomerDTO.java
+++ src/main/java/kz/ks/storefront/addressbook/controller/dto/PersistentCustomerDTO.java
@@ -1,12 +1,22 @@
 package kz.ks.storefront.addressbook.controller.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Builder
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class PersistentCustomerDTO {
+    @Schema(
+            description = "Customer ID",
+            example = "87098664732"
+    )
     private long id;
+
+    @Schema(
+            description = "Global Customer ID",
+            example = "9761245"
+    )
     private String gci;
 }
